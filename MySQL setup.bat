52e2fc4eb18e8d7757f1d95a06f36a9f7989b9f9
lyhottag13 <leonardo@promoevt.com>
2025-07-07 09:28:58 -0700

    Updated package-lock.json

--- MySQL setup.bat
+++ MySQL setup.bat
@@ -14,7 +14,7 @@ where npm >nul 2>&1
 if NOT %errorlevel%==0 (
 	echo Install npm and Node.js!
 ) else (
-	npm.cmd install next@15.3.4 exceljs@4.4.0 @mui/material@7.2.0 @emotion/react@11.14.0 @emotion/styled@11.14.1
+	npm.cmd ci
 	powershell -Command "(Get-Content prisma\schema.prisma) -replace 'sqlserver', 'mysql' | Set-Content prisma\schema.prisma"
 	npx prisma db pull --force
 	npx prisma generate
