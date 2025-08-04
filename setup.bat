a3080c7ccb919119fbab3752221967e85af01e04
lyhottag13 <leonardo@promoevt.com>
2025-08-04 10:05:23 -0700

    Update setup.bat

--- setup.bat
+++ setup.bat
@@ -4,7 +4,7 @@ SETLOCAL ENABLEDELAYEDEXPANSION
 cd Sistema420-2025-main
 :: Clean installs everything so that there's less risk with dependencies updating.
 echo Clean installing node_modules...
-CALL npm.cmd ci
+CALL npm ci
 :: Generates the prisma tool from the schema.
 echo Generating Prisma...
 CALL npx prisma db pull --force
@@ -16,20 +16,8 @@ CALL npx tailwindcss -i ./styles/index.css -o ./public/tailwind.css
 CALL npm run build
 :: Adds the app daemon to pm2.
 set /p newport=What's the desired localhost port? 
-echo Generating web.config and adding process to pm2...
-echo ^<?xml version="1.0" encoding="UTF-8"?^> > web.config
-echo ^<configuration^> >> web.config
-echo  ^<system.webServer^> >> web.config
-echo  ^<rewrite^> >> web.config
-echo  ^<rules^> >> web.config
-echo  ^<rule name="ReverseProxyInboundRule1" stopProcessing="true"^> >> web.config
-echo  ^<match url="(.*)" /^> >> web.config
-echo  ^<action type="Rewrite" url="http://localhost:!newport!/{R:1}" /^> >> web.config
-echo  ^</rule^> >> web.config
-echo  ^</rules^> >> web.config
-echo  ^</rewrite^> >> web.config
-echo  ^</system.webServer^> >> web.config
-echo ^</configuration^> >> web.config
+echo Adding process to pm2...
 CALL pm2 delete sys420
 CALL pm2 start node_modules/next/dist/bin/next --name "sys420" -- start -p !newport!
 CALL pm2 save
+PAUSE
